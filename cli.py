7c3d69c1cd6d25358126613deb40ec2a379d578f
Janos Mezo <jmezo@dataedge.hu>
2021-03-06 12:27:44 +0100

    switched from click to typer

--- cli.py
+++ cli.py
@@ -1,28 +1,20 @@
-import click
+import typer
 
 from app import web3_client
 
-
-@click.group()
-def main():
-    """
-    web3 cli tool
-    """
-    pass
+app = typer.Typer()
 
 
-@main.command()
+@app.command()
 def addresses():
-    """Returns eth account addresses"""
-    click.echo(web3_client.get_accounts())
+    typer.echo(web3_client.get_accounts())
 
 
-@main.command()
-@click.argument("address")
-def balance(address):
+@app.command()
+def balance(address: str):
     """Returns balance of an address"""
-    click.echo(web3_client.get_eth_balance(address))
+    typer.echo(web3_client.get_eth_balance(address))
 
 
 if __name__ == "__main__":
-    main()
+    app()
