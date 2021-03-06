93294fe01be78885c8a68942599f19b03da38ca6
Janos Mezo <jmezo@dataedge.hu>
2021-03-06 16:32:42 +0100

    removed db, added optional db uri param for creating crud instance

--- app/crud.py
+++ app/crud.py
@@ -1,17 +1,27 @@
-from typing import Any
+from typing import Any, Optional
+from urllib.parse import urlparse
 
-from app.db import etcd
+import etcd3
+
+from app.config import config
 
 
 class Crud:
+    def __init__(self, db_uri: Optional[str] = None):
+        if not db_uri:
+            db_host = config.DB_HOST
+            db_port = config.DB_PORT
+        else:
+            parsed_uri = urlparse(db_uri)
+            db_host = parsed_uri.hostname
+            db_port = str(parsed_uri.port)
+        self.db = etcd3.client(host=db_host, port=db_port)
+
     def get(self, key: str) -> Any:
-        return etcd.get(key)
+        return self.db.get(key)
 
     def put(self, key: str, value: Any) -> None:
-        etcd.put(key, value)
+        self.db.put(key, value)
 
     def delete(self, key: str) -> None:
-        etcd.delete(key)
-
-
-crud = Crud()
+        self.db.delete(key)
--- app/db.py
+++
@@ -1,5 +0,0 @@
-import etcd3
-
-from app.config import config
-
-etcd = etcd3.client(host=config.DB_HOST, port=config.DB_PORT)
