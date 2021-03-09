34548bcb06d580ccbf28e9d5db09ffb3ce731b4f
Janos Mezo <jmezo@dataedge.hu>
2021-03-09 21:37:14 +0100

    added web api and web server

--- app/api.py
+++ app/api.py
@@ -1,1 +1,22 @@
-# TODO implement
+import os
+
+from fastapi import FastAPI
+
+from app.crud import Crud
+from app.web3_client import get_w3
+
+w3 = get_w3(os.getenv("WEB3_PROVIDER"))
+crud = Crud(os.getenv("DB_URI"))
+
+
+app = FastAPI()
+
+
+@app.get("/addresses/{address}")
+async def find_balances(address: str):
+    return {"message": "To be implemented."}
+
+
+@app.get("/session/{id}")
+async def get_session(id: str):
+    return {"message": "To be implemented."}
