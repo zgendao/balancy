c65817863cfc67d826dcfb548a09fa504e48654e
Janos Mezo <jmezo@dataedge.hu>
2021-03-20 16:58:40 +0100

    added function to get balance of address in token

--- app/balances.py
+++ app/balances.py
@@ -1,7 +1,24 @@
 from web3 import Web3
 
-from app.crud import Crud
+ABI_FOR_BALANCE_OF = [
+    {
+        "constant": True,
+        "inputs": [{"name": "who", "type": "address"}],
+        "name": "balanceOf",
+        "outputs": [{"name": "", "type": "uint256"}],
+        "payable": False,
+        "stateMutability": "view",
+        "type": "function",
+    },
+]
 
 
-def get_token_balance(address: str, token: str, *, w3: Web3, crud: Crud) -> int:
-    ...
+def balance_of_address(address: str, token_address: str, *, w3: Web3) -> int:
+    try:
+        cs_address = w3.toChecksumAddress(address)
+        token_cs_address = w3.toChecksumAddress(token_address)
+    except ValueError:
+        return 0
+    contract = w3.eth.contract(address=token_cs_address, abi=ABI_FOR_BALANCE_OF)
+    res = contract.functions.balanceOf(cs_address).call()
+    return res
